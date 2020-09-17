06fb5a1ff7029df13a1bc32d9565dfb035d5824c
Andrusv <andresbarrosocl@gmail.com>
2020-09-16 21:08:47 -0400

    Cree la carpeta de Utils/auth/strategies y anadi la estrategia basic.js

---
+++ utils/auth/strategies/basic.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const { BasicStrategy } = require('passport-http');
+const boom = require('@hapi/boom');
+const bcryptjs = require('bcryptjs');
+
+const UsersService = require('../../../services/users');
+
+passport.use(
+  new BasicStrategy(async function(email, password, cb) {
+    const userService = new UsersService();
+
+    try {
+      const user = await userService.getUser({ email });
+
+      if (!user) {
+        return cb(boom.unauthorized(), false);
+      }
+
+      if (!(await bcryptjs.compare(password, user.password))) {
+        return cb(boom.unauthorized(), false);
+      }
+
+      delete user.password;
+
+      return cb(null, user);
+    } catch (error) {
+      return cb(error);
+    }
+  })
+);
